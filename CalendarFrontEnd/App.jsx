1019e9108322e1e4da250842a262559fa1319cb3
Bderiel <bderiel@gmail.com>
2018-04-19 22:27:01 -0400

    months now start with correct date

--- CalendarFrontEnd/App.jsx
+++ CalendarFrontEnd/App.jsx
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import { Day } from './components';
 
+let shortid = require('shortid');
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
       events: [],
-      currentMonth: 4,
+      currentMonth: 3,
       currentYear: 2018,
+      daysInMonth: [],
     };
     this.months = this.months.bind(this);
     this.daysInMonth = this.daysInMonth.bind(this);
@@ -16,8 +19,10 @@ class App extends Component {
   }
   componentDidMount() {
     // grab all events put on state
+    const { currentMonth, currentYear } = this.state;
+    this.daysInMonth(currentMonth, currentYear);
   }
-  months() {
+  months() { // make utils
     return ['January',
       'Feburary',
       'March',
@@ -34,15 +39,19 @@ class App extends Component {
 
   daysInMonth(month, year) {
     const days = new Date(year, month, 0).getDate();
-    const output = [];
-    for (let i = 1; i <= 35; i++) {
+    const FirstDay = new Date(year, month, 1).getDay();
+    let daysInMonth = [] // fill array to account for day that month starts with
+
+    for (let i = 0; i < FirstDay; i++) daysInMonth.push({ day: '', dark: true, _id: shortid.generate() });
+    
+    for (let i = 1; i <= 35 - FirstDay; i++) {
       if (i <= days) {
-        output.push({ day: i });
+        daysInMonth.push({ day: i, _id: shortid.generate() });
       } else {
-        output.push({ day: i % days, dark: true });
+        daysInMonth.push({ day: '', dark: true, _id: shortid.generate() });
       }
     }
-    return output;
+    this.setState({ daysInMonth });
   }
   handleClick(action) {
     let { currentMonth, currentYear } = this.state;
@@ -51,42 +60,42 @@ class App extends Component {
         this.setState({
           currentMonth: 0,
         });
+        this.daysInMonth(0, currentYear);
       } else {
         this.setState({
           currentMonth: currentMonth += 1,
         });
+        this.daysInMonth(currentMonth, currentYear);
       }
     } else if (currentMonth === 0) {
       this.setState({
         currentMonth: 11,
       });
+      this.daysInMonth(11, currentYear);
     } else {
       this.setState({
         currentMonth: currentMonth -= 1,
       });
+      this.daysInMonth(currentMonth, currentYear);
     }
   }
   render() {
     const month = this.months();
-    const { currentMonth, currentYear } = this.state;
-    console.log(currentMonth, 'currentMonth');
-    const days = this.daysInMonth(currentMonth, currentYear);
+    const { currentMonth, currentYear, daysInMonth } = this.state;
+    console.log(daysInMonth);
     return (
       <div className="container">
-
         <div className="header">
+          <i onClick={() => (this.handleClick('left'))} className="fa fa-arrow-left" />
           <div>{month[currentMonth]}<span> {currentYear}  </span>
-            <i onClick={() => (this.handleClick('left'))} className="fa fa-arrow-left" />
             <i onClick={() => (this.handleClick('right'))} className="fa fa-arrow-right" />
           </div>
         </div>
-
-
         <div className="flex calendar">
-          {days.map((day, idx) => {
-            console.log(1);
+          {daysInMonth.length && daysInMonth.map((day, idx) => {
+            const test = ['this is a test', 'what is life?', 'okay', 'testing'];
             return (
-              <Day dark={day.dark} currentDay={day.day} key={days[idx].day} />
+              <Day events={test} dark={day.dark} currentDay={day.day} key={day._id} />
             );
           })}
         </div>
--- CalendarFrontEnd/components/Day.jsx
+++ CalendarFrontEnd/components/Day.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 
 const Day = (props) => {
-  const { currentDay, dark } = props;
+  const { currentDay, dark, events } = props;
+  const truncate = events.slice(0, 3);
   return (
     dark ?
-      <div className="date dark">{currentDay}</div> :
-      <div className="date">{currentDay}</div>
+      <div className="date dark">{currentDay}
+      </div> :
+      <div className="date">{currentDay}
+        {truncate.map((event) => {
+          return (<p>{event.slice(0, 15)}</p>);
+        })}
+      </div>
   );
 };
 
